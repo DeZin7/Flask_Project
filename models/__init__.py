ff7771b85423336e14f05d8ae0127bdd09406591
DeZin7 <marcusandre77@icloud.com>
2023-01-09 16:43:07 -0800

    added many-to-many relationships with SQLAlchemy

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,6 @@
 from models.store import StoreModel
 from models.item import ItemModel
+from models.tag import TagModel
+from models.item_tags import ItemTags
 
 #created for we dont need to make "from model.item import ItemModel"
--- schemas.py
+++ schemas.py
@@ -1,13 +1,17 @@
 from marshmallow import Schema, fields
 
 class PlainItemSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
    
 
 class PlainStoresSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
 
 
@@ -20,8 +24,20 @@ class ItemUpdateSchema(Schema):
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoresSchema(), dump_only=True) #when return data to the client, not when receiving data from the client
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 class StoresSchema(PlainStoresSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
     #using PlainStoresSchemas and PLainItemSchema so that when we use  nesting, we can only include a part of the fields
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoresSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
